3819a8ffcd60a1c87cf7d32a72e46aa2b482d0fd
Wdrm <126005544+Wdrm@users.noreply.github.com>
2023-04-07 08:59:56 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Qual Operação Vamos Realizar\n" +
+                    "0 Encerra\n" +
+                    "1 Àrea e Perímetro do Retângulo\n"+
+                    "2 Área e Perímetro do Quadrado");
+
+                int opcao = sc.nextInt();
+        switch (opcao){
+            case 0:
+                break;
+            case 1: Diversos div = new Diversos(); div.retangulo();
+            break;
+            case 2:Diversos dive = new Diversos();dive.quadrado();
+            break;
+
+         }
+
+
+
+
+
+
+    }
+}
